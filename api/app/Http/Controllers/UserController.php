31b0b19eb4971a42a4a0a82303ae1925dabf5b17
Abisai Emmanuel Martinez Olmos <robotemma88@gmail.com>
2023-03-14 19:24:55 -0600

    register

--- api/app/Http/Controllers/UserController.php
+++ api/app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -10,7 +11,8 @@ class UserController extends Controller
      * registry
      */
 
-    public function register(Request $request){
+    public function register(Request $request)
+    {
         //Recoger post
         $json = $request->get('json');
         $params = json_decode($json, null);
@@ -21,14 +23,54 @@ class UserController extends Controller
         $surname = (!is_null($json) && isset($params->surname)) ? $params->surname : null;
         $role = 'Role_User';
 
+        if (!isnull($email) && !is_null($password) && !is_null($name)) {
+            //crear usuario
+            $user = new User();
+            $user->email = $email;
+            $user->name = $name;
+            $user->surname = $name;
+            $user->role = $role;
 
+            $pwd = hash('sha256', $password);
+            $user->password = $pwd;
+
+            // Comprobar usuario duplicado
+            $isset_user = User::where('email', '=', $email)->firts();
+
+            if (count($isset_user) == 0) {
+                //Guardar el usuario
+                $user->save();
+                $data = array(
+                    'status' => 'success',
+                    'code' => 200,
+                    'message' => 'Usuario registrado correctamente'
+                );
+            } else {
+                //Usuario duplicado
+                $data = array(
+                    'status' => 'error',
+                    'code' => 400,
+                    'message' => 'Usuario duplicado'
+                );
+            }
+
+        } else {
+            $data = array(
+                'status' => 'error',
+                'code' => 400,
+                'message' => 'Usuario no creado'
+            );
+        }
+        return response()->json($data, 200);
     }
 
     /**
      * login
      */
 
-    public function login(){
-        echo 'login'; die();
+    public function login()
+    {
+        echo 'login';
+        die();
     }
 }
